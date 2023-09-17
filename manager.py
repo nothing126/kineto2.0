34a73ca71bc7a6e40fd0cda84dccabcefd926a76
nothing126 <131160147+nothing126@users.noreply.github.com>
2023-09-17 18:57:47 +0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -4,33 +4,37 @@ from telebot import types
 import config
 from yandex import send_message
 
-
+#удаление клавиатуры с кнопками 
 def remove():
     markup = types.ReplyKeyboardRemove(selective=False)
     return markup
 
-
+#класс бота 
 class BotManager:
+#инициализация класса 
     def __init__(self, token):
-        self.bot = telebot.TeleBot(token)
+        self.bot = telebot.TeleBot(token )#инициализация переменной для обращения к боту
         self.user_data = {}  # Словарь для хранения данных пользователей
 
     def start(self):
+#реакция на команду/start
         @self.bot.message_handler(commands=['start'])
         def start(message):
             chat_id = message.chat.id
-
+#созданик клавиатуры 
             markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-
+#создание кнопок 
             button1 = types.KeyboardButton("Русский")
             button2 = types.KeyboardButton("Romana")
 
+#добавление кнопок на клавиатуру
             markup.add(button1)
             markup.add(button2)
-
+#приветствие
             self.bot.send_message(chat_id, "Здравствуйте, выберите язык/Buna ziua,selectati limba:",
                                   reply_markup=markup)
 
+#функция при выборе кнопки русского языка 
         @self.bot.message_handler(func=lambda message: message.text == "Русский")
         def change_ru(message):
             chat_id = message.chat.id
@@ -39,6 +43,7 @@ class BotManager:
                                   reply_markup=remove())
             self.bot.register_next_step_handler(message, self.cause_ru)
 
+#функция выбора румынского языка 
         @self.bot.message_handler(func=lambda message: message.text == "Romana")
         def change_ru(message):
             chat_id = message.chat.id
@@ -47,6 +52,7 @@ class BotManager:
                                   reply_markup=remove())
             self.bot.register_next_step_handler(message, self.cause_ro)
 
+#реакция на нажатие кнопки "существующие мед.проблемы"
         @self.bot.message_handler(func=lambda message: message.text == "Существующие медицинские проблемы.")
         def problem_ru(message):
             chat_id = message.chat.id
@@ -56,9 +62,10 @@ class BotManager:
             self.bot.send_message(chat_id, f"{user_name}, если у вас есть существующие медицинские проблемы, "
                                            "пожалуйста, расскажите о них подробнее",
                                   reply_markup=remove())
-
+#слелующий. шаг
             self.bot.register_next_step_handler(message, self.save_medical_problem_ru)
 
+#нажатие на кнопку "существующие мед.проблемы" на румынском 
         @self.bot.message_handler(func=lambda message: message.text == "Probleme medicale existente.")
         def problem_ru(message):
             chat_id = message.chat.id
@@ -71,6 +78,7 @@ class BotManager:
 
             self.bot.register_next_step_handler(message, self.save_medical_problem_ro)
 
+#реакция на нажатие кнопки "дискомфорт в спине и суставах 
         @self.bot.message_handler(
             func=lambda message: message.text == "Дискомфорт или беспокойство в области спины или суставов.")
         def discomfort_ru(message):
@@ -81,9 +89,9 @@ class BotManager:
             self.bot.send_message(chat_id, f"{user_name},Пожалуйста, опишите характер дискомфорта и "
                                            f"его локализацию более подробно.",
                                   reply_markup=remove())
-
+#следующий шаг
             self.bot.register_next_step_handler(message, self.reab_ru)
-
+#реакция на "проблемы в области спины и суставов на румынском"
         @self.bot.message_handler(
             func=lambda message: message.text == "Disconfort sau neliniște în spate sau articulații.")
         def discomfort_ru(message):
@@ -97,6 +105,7 @@ class BotManager:
 
             self.bot.register_next_step_handler(message, self.reab_ro)
 
+#реакция на кнопку "профилактика и поддержание"
         @self.bot.message_handler(
             func=lambda message: message.text == "Профилактика и поддержание физической активности.")
         def problem_ru(message):
@@ -117,6 +126,7 @@ class BotManager:
                                   reply_markup=markup)
             self.bot.register_next_step_handler(message, self.registrForProf_ru)
 
+#реакция на кнопку "профилактика и поддержание" на румынском 
         @self.bot.message_handler(
             func=lambda message: message.text == "Prevenirea și menținerea activității fizice.")
         def problem_ro(message):
@@ -137,6 +147,7 @@ class BotManager:
                                   reply_markup=markup)
             self.bot.register_next_step_handler(message, self.registrForProf_ro)
 
+# реакция на подтверждение регистрации 
         @self.bot.message_handler(func=lambda message: message.text == "Да")
         def confirm_prof_ru(message):
             chat_id = message.chat.id
@@ -149,6 +160,7 @@ class BotManager:
 
             self.bot.register_next_step_handler(message, self.sendmess1_ru)
 
+#подтверждение регистрации на румынском 
         @self.bot.message_handler(func=lambda message: message.text == "Da")
         def confirm_prof_ro(message):
             chat_id = message.chat.id
@@ -161,6 +173,7 @@ class BotManager:
 
             self.bot.register_next_step_handler(message, self.sendmess1_ro)
 
+#подтверждение регистрации 
         @self.bot.message_handler(func=lambda message: message.text == "Да,хочу записаться")
         def confirm_prob_ru(message):
             chat_id = message.chat.id
@@ -173,6 +186,7 @@ class BotManager:
 
             self.bot.register_next_step_handler(message, self.sendmess2_ru)
 
+#подтверждение регистрации на румынском 
         @self.bot.message_handler(func=lambda message: message.text == "Da, vreau să mă înscriu")
         def confirm_prob_ro(message):
             chat_id = message.chat.id
@@ -185,6 +199,7 @@ class BotManager:
 
             self.bot.register_next_step_handler(message, self.sendmess2_ro)
 
+#подтвержденик регистрации 
         @self.bot.message_handler(func=lambda message: message.text == "Да,готов записаться")
         def confirm_prob_ru(message):
             chat_id = message.chat.id
@@ -197,6 +212,7 @@ class BotManager:
 
             self.bot.register_next_step_handler(message, self.sendmess3_ru)
 
+#подтверждение регистрации на румынском 
         @self.bot.message_handler(func=lambda message: message.text == "Da, sunt gata să mă înscriu")
         def confirm_prob_ru(message):
             chat_id = message.chat.id
@@ -218,6 +234,7 @@ class BotManager:
                                            "nu ezitați să contactați.La revedere și sănătate",
                                   reply_markup=remove())
 
+#отказ от регистрации 
         @self.bot.message_handler(func=lambda message: message.text == "Нет, спасибо")
         def confirm_prob_ru(message):
             chat_id = message.chat.id
@@ -229,12 +246,14 @@ class BotManager:
 
         self.bot.polling()
 
+#обрабртка выбора языка
     def language_handler(self, message):
         if message.text == "Romana":
             self.bot.register_next_step_handler(message, self.cause_ro)
         elif message.text == "Русский":
             self.bot.register_next_step_handler(message, self.cause_ru)
 
+#предложение регистрации 
     def reab_ru(self, message):
         discomfort = message.text
         chat_id = message.chat.id
@@ -257,6 +276,7 @@ class BotManager:
                               f"определения наилучшего способа помощи. Готовы ли вы записаться на консультацию?",
                               reply_markup=markup)
 
+#предложение регистрации на румынском 
     def reab_ro(self, message):
         discomfort = message.text
         chat_id = message.chat.id
@@ -279,7 +299,7 @@ class BotManager:
                               f"pentru a determina cea mai bună modalitate de a ajuta. "
                               f"Sunteți gata să programați o consultație?",
                               reply_markup=markup)
-
+#выбор причины обращения 
     def cause_ru(self, message):
         chat_id = message.chat.id
         user_id = message.from_user.id
@@ -300,6 +320,7 @@ class BotManager:
         self.bot.send_message(chat_id, f"Что привело вас сегодня, {user_name} ?",
                               reply_markup=markup)
 
+#выбор причины обращения на румынском 
     def cause_ro(self, message):
         chat_id = message.chat.id
         user_id = message.from_user.id
@@ -320,6 +341,7 @@ class BotManager:
         self.bot.send_message(chat_id, f"Ce vă aduce astăzi, {user_name} ?",
                               reply_markup=markup)
 
+#предложение регистрации 
     def registrForProf_ru(self, message):
         chat_id = message.chat.id
 
@@ -333,7 +355,7 @@ class BotManager:
 
         self.bot.send_message(chat_id, "Желаете ли вы записаться на процедуру?",
                               reply_markup=markup)
-
+#предложение регистрации на румынском 
     def registrForProf_ro(self, message):
         chat_id = message.chat.id
 
@@ -348,6 +370,7 @@ class BotManager:
         self.bot.send_message(chat_id, "Doriți să faceți o programare pentru procedură?",
                               reply_markup=markup)
 
+#получение мед. проблемы 
     def save_medical_problem_ru(self, message):
         chat_id = message.chat.id
         user_id = message.from_user.id
@@ -371,6 +394,7 @@ class BotManager:
         self.bot.send_message(chat_id, "Желаете ли вы записаться на процедуру?",
                               reply_markup=markup)
 
+#получение мед. проблемы на румынском 
     def save_medical_problem_ro(self, message):
         chat_id = message.chat.id
         user_id = message.from_user.id
@@ -394,6 +418,7 @@ class BotManager:
         self.bot.send_message(chat_id, "Doriți să faceți o programare pentru procedură?",
                               reply_markup=markup)
 
+#отправка данных пользователя на почту и благодарность 
     def sendmess1_ru(self, message):
         phone_num = message.text
         chat_id = message.chat.id
@@ -409,6 +434,7 @@ class BotManager:
                                        f"Я свяжусь с вами в ближайшее время для уточнения деталей.",
                               reply_markup=remove())
 
+#отправка данных пользователя на почту и благодарность на рум
     def sendmess1_ro(self, message):
         phone_num = message.text
         chat_id = message.chat.id
@@ -424,6 +450,7 @@ class BotManager:
                                        f"Vă voi contacta în scurt timp pentru a clarifica detalii.",
                               reply_markup=remove())
 
+#отправка данных пользователя на почту и благодарность 
     def sendmess2_ru(self, message):
         phone_num = message.text
         chat_id = message.chat.id
@@ -439,6 +466,8 @@ class BotManager:
                                        f"Я свяжусь с вами в ближайшее время для уточнения деталей.",
                               reply_markup=remove())
 
+
+#отправка данных пользователя на почту и благодарность на рум
     def sendmess2_ro(self, message):
         phone_num = message.text
         lang = "romana"
@@ -455,7 +484,8 @@ class BotManager:
                                        f"Vă voi contacta în scurt timp pentru a clarifica detalii.",
                               reply_markup=remove())
 
-    def sendmess3_ru(self, message):
+
+#отправка данных пользователя на почту и благодарность     def sendmess3_ru(self, message):
         lang = "русский"
         phone_num = message.text
         chat_id = message.chat.id
@@ -473,6 +503,7 @@ class BotManager:
                                        f"Я свяжусь с вами в ближайшее время для уточнения деталей.",
                               reply_markup=remove())
 
+#отправка данных пользователя на почту и благодарность на рум
     def sendmess3_ro(self, message):
         lang = "romana"
         phone_num = message.text
